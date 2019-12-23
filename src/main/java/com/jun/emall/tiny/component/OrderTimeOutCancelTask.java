a5937374cbccbc132d3e9b41b620271809206921
Vincent Shen <airseej@163.com>
2019-12-23 17:02:10 +0800

    Integrate SpringTask

---
+++ src/main/java/com/jun/emall/tiny/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.jun.emall.tiny.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderTimeOutCancelTask {
+
+    private Logger LOGGER = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+
+    @Scheduled(cron = "0/10 * * ? * ?")
+    private void cancelTimeOutOrder() {
+        LOGGER.info("取消订单，并根据sku编号释放锁定库存。S");
+    }
+}
